22f968c3e591a61ec918cd43ba056da69193d962
MayurNamdev004 <namdevmayur004@gmail.com>
2024-09-14 10:15:55 +0530

    updation..

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const user = require("./routes/user");
+app.use(user);
+
+
+
+
+app.listen(3001, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log("server is running")
+    }
+})
+//localhost:3000/users
